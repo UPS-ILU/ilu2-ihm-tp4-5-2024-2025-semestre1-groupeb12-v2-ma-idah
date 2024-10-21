578cd4b6d2cdbe454ce3431e1acbe170496ae419
ma-idah <maidasalifou@gmail.com>
2024-10-21 17:42:29 +0200

    Livrable 3 version finale

--- src/main/java/interface_noyau_fonctionnel/InterfaceNoyauFonctionnel.java
+++ src/main/java/interface_noyau_fonctionnel/InterfaceNoyauFonctionnel.java
@@ -1,3 +1,4 @@
+//tu dois 
 package interface_noyau_fonctionnel;
 
 public class InterfaceNoyauFonctionnel {
@@ -5,5 +6,45 @@ public class InterfaceNoyauFonctionnel {
     public String[] trouverTableDisponible(int jour, int mois, int nombrePersonnes, String time) {
         return new String[]{"Table 1", "Table 2", "Table 3", "Table 4", "Table 5", "Table 6"};
     }
+    
+    //méthode permettant de generer une liste de tables adaptée au nombre de personnes choisies
+    public String[] tablesDisponibles(int nbPersonnes){
+        if(nbPersonnes == 2 ){
+            return new String[]{"Table 1"};
+            }
+        if(nbPersonnes == 3 || nbPersonnes == 4){
+            return new String[]{"Table 2", "Table 3", "Table 4", "Table 5"};
+            }
+            
+        if(nbPersonnes == 5 || nbPersonnes == 6 ||nbPersonnes == 7 || nbPersonnes == 8){
+            return new String[]{"Table 6"};
+            }
+        return null;
+         }
+    
+    
 
+//methodes appelées dans le dialog
+    public void dateChoisie(){
+        System.out.println("la date a été choisie");
+    }
+    
+    
+    public void heureChoisie(){
+        System.out.println("l'heure a été choisie");
+    }
+    
+    
+    public void tableChoisie(int n){
+        System.out.println( " la table " + n + " a été choisie");
+    }
+    public void confirmation(){
+        System.out.println( " l'utilisateur a validé la réservation");
+    }
+    
+    public void annuler(){
+        System.out.println( " l'utilisateur a annulé la réservation");
+    }
+    
 }
+
